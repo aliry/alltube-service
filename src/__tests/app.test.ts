421a3c904ad9e209e40661bae7fdc169eb0c924f
Ali.Dev <aliry@live.com>
2022-11-13 16:29:31 -0800

    add more tests

--- src/__tests/app.test.ts
+++ src/__tests/app.test.ts
@@ -1,12 +1,23 @@
 import request from 'supertest';
 import app from '../app';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.development' });
 
 describe('App Test', () => {
+  const headers = {
+    'API-KEY': process.env.API_KEY
+  };
+
   test('GET /random-url should return 404', (done) => {
-    request(app).get('/reset').expect(404, done);
+    request(app).get('/reset').set(headers).expect(404, done);
   });
 
   test('GET root route should return 200', (done) => {
-    request(app).get('/').expect(200, done);
+    request(app).get('/').set(headers).expect(200, done);
+  });
+
+  test('GET with no API_KEY return 401 ', (done) => {
+    request(app).get('/').expect(401, done);
   });
 });
